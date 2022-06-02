c1bd6edb02e945170701ecc0f122a632bf3c6721
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-06-02 17:37:12 +0200

    make payment production

--- app/Http/Controllers/MyfatoorahController.php
+++ app/Http/Controllers/MyfatoorahController.php
@@ -34,8 +34,8 @@ class MyfatoorahController extends Controller
         $resturant = Restaurant::findOrFail(session('resturant_id'));
         $price = session('new_price');
         $root_url = $request->root();
-        $path = 'https://apitest.myfatoorah.com/v2/SendPayment';
-        $token ='bearer rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL';
+        $path = 'https://api.myfatoorah.com/v2/SendPayment';
+        $token ='bearer 4OS8zrCmq9CsqWiXkpjiZ7SVH-b8Ral3_Ty1j5JL6AwBMiK75ku0LDa4ZAf0hRIJsCK5qJQyJlkMrc1m5UzugnzVCFNCfrXr5xmwzT4ERhgXZqTe-aM9_zrAL7_lvMkFDqR5sstMOsqKW4Q0qKUQSX2nDiDpuK9dhAjWxE2cTazDANb7f5ZK3lAs3Fb9oODP7hW24huAZZ3u4kB36I5MslrSKL5zGdS15yG2E8tw02PKXI7VSL2c1o0jLsNtiiRcK5aIuQJAD6nd-9g3mjhc2T35LnFf5uonSz7RKbpDVwwVlCrPvEn7-986Pn3QotYlK-JJzyhmyYUIgEQnXIUmJDa2NVuxHFAFh80IUEjP0BsrqNacdD3nxW4bgKrxzdKHdtidbTFj2ITwT4I_kY9NNhdgWZFDnK5pWnOaBFMGNaPcXWOSJCWhU1yuADRy6qcbXCXTvKRM1A6YbzZBODrmz5nywepK2zEQhbSFsP_U2KOI-2IrZ3qMR9ZLk3WyoM9dCGXd0KXj4Vz72T4PS9p8Z9xcLcOpH2fq3S0yFW3GqRxNJKW5O25G58M7ZrsLxOo6ZyPPxDWypwd_wVaV1y-VatnkcvYlmLlQAZtedJ4pu1192T-ADkyPb7ITTPT5IpGNTMfRykxw8Ex0xXCaqFJln7X6f-XbYe5H2KvOw2IaCwOD5pQWeIsBbfWY2BBtTq2bnXvOxA';
         $headers = array(
             'Authorization:' . $token,
             'Content-Type:application/json'
@@ -106,8 +106,8 @@ class MyfatoorahController extends Controller
         $order = Order::findOrFail(session('order_id'));
         $price = $order->order_amount;
         $root_url = $request->root();
-        $path = 'https://apitest.myfatoorah.com/v2/SendPayment';
-        $token = 'bearer rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL';
+        $path = 'https://api.myfatoorah.com/v2/SendPayment';
+        $token = 'bearer 4OS8zrCmq9CsqWiXkpjiZ7SVH-b8Ral3_Ty1j5JL6AwBMiK75ku0LDa4ZAf0hRIJsCK5qJQyJlkMrc1m5UzugnzVCFNCfrXr5xmwzT4ERhgXZqTe-aM9_zrAL7_lvMkFDqR5sstMOsqKW4Q0qKUQSX2nDiDpuK9dhAjWxE2cTazDANb7f5ZK3lAs3Fb9oODP7hW24huAZZ3u4kB36I5MslrSKL5zGdS15yG2E8tw02PKXI7VSL2c1o0jLsNtiiRcK5aIuQJAD6nd-9g3mjhc2T35LnFf5uonSz7RKbpDVwwVlCrPvEn7-986Pn3QotYlK-JJzyhmyYUIgEQnXIUmJDa2NVuxHFAFh80IUEjP0BsrqNacdD3nxW4bgKrxzdKHdtidbTFj2ITwT4I_kY9NNhdgWZFDnK5pWnOaBFMGNaPcXWOSJCWhU1yuADRy6qcbXCXTvKRM1A6YbzZBODrmz5nywepK2zEQhbSFsP_U2KOI-2IrZ3qMR9ZLk3WyoM9dCGXd0KXj4Vz72T4PS9p8Z9xcLcOpH2fq3S0yFW3GqRxNJKW5O25G58M7ZrsLxOo6ZyPPxDWypwd_wVaV1y-VatnkcvYlmLlQAZtedJ4pu1192T-ADkyPb7ITTPT5IpGNTMfRykxw8Ex0xXCaqFJln7X6f-XbYe5H2KvOw2IaCwOD5pQWeIsBbfWY2BBtTq2bnXvOxA';
         $headers = array(
             'Authorization:' . $token,
             'Content-Type:application/json'
